eaadaa14c8fb4fa8d71340242ec8b369bd8d58bc
lingyicute <160479206+lingyicute@users.noreply.github.com>
2024-02-19 20:43:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const elements = document.querySelectorAll('.avatar, h1, .motto, .nav-buttons');
-  for (let i = 0; i < elements.length; i++) {
+  const elements = document.querySelectorAll('.avatar, h1, .motto, .button');
+  let delay = 0;
+  elements.forEach(el => {
     setTimeout(() => {
-      elements[i].style.opacity = 1;
-    }, i * 500);
-  }
+      el.classList.add('fade-in');
+    }, delay);
+    delay += 500; // Increment the delay for the next element
+  });
 });
